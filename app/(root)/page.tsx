5648d7305c09a78c37e57f5919633841763620d0
Diego Robles <hello@d1360.cl>
2025-04-19 07:26:16 +0800

    feat(main): added startupcard and sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "../../components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,22 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: "Elon Musk",
+        image: "https://placehold.co/150x150",
+      },
+      _id: 1,
+      description: "This is a description",
+      image: "https://placehold.co/600x400",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <div>
       <section className="pink_container">
@@ -19,6 +36,20 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups found</p>
+          )}
+        </ul>
       </section>
     </div>
   );
