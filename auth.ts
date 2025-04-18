580234ea1b03aba812cfe7c6dce258622f41f1cc
Diego Robles <hello@d1360.cl>
2025-04-19 01:43:04 +0800

    feat(main): added next-auth with github provider and navbar

---
+++ auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+    }),
+  ],
+});
